2a1befc047fabd0bfa9b7a641748de3b09dc6fbd
Himanshu Patel <himanshu.patel_cs19@gla.ac.in>
2022-08-11 10:49:16 +0530

    done with v1

--- popup.js
+++ popup.js
@@ -1,38 +1,54 @@
-const btn = document.querySelector(".changeColorBtn");
-const colorGrid = document.querySelector(".colorGrid");
-const colorValue = document.querySelector(".colorValue");
-
-btn.addEventListener("click", async () => {
-  chrome.storage.sync.get("color", ({ color }) => {
-    console.log("color: ", color);
+const getContest = async () => {
+  let data;
+  try {
+    const res = await fetch(
+      `https://clist.by/api/v2/contest/?username=user_name&api_key=api_key&upcoming=true&ongoing=true&format_time=true&order_by=resource_id&limit=500`
+    );
+    const result = await res.json();
+    data = result.objects;
+    console.log(data);
+  } catch (error) {}
+  const fruits = [];
+  data?.map((index) => {
+    fruits.push(index);
   });
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  console.log(fruits);
+  const tableBody = document.getElementById("table-body");
 
-  chrome.scripting.executeScript(
-    {
-      target: { tabId: tab.id },
-      function: pickColor,
-    },
-    async (injectionResults) => {
-      const [data] = injectionResults;
-      if (data.result) {
-        const color = data.result.sRGBHex;
-        colorGrid.style.backgroundColor = color;
-        colorValue.innerText = color;
-        try {
-          await navigator.clipboard.writeText(color);
-        } catch (err) {}
+  for (const user of fruits) {
+    const tr = document.createElement("tr");
+    const content = `<td>${user.event}</td>
+      <td>${user.start}</td>
+      <td>${user.end}</td>
+      <td>${user.host}</td>
+      <td>${user.href}</td>`;
+    tr.innerHTML = content;
+    tableBody?.appendChild(tr);
+  }
+
+  document.getElementById("myInput").addEventListener("keypress", myFunction);
+  function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value;
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[3];
+      console.log(td);
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
       }
     }
-  );
-});
-
-async function pickColor() {
-  try {
-    // Picker
-    const eyeDropper = new EyeDropper();
-    return await eyeDropper.open();
-  } catch (err) {
-    console.error(err);
   }
-}
+};
+
+window.addEventListener("load", () => {
+  getContest();
+});
